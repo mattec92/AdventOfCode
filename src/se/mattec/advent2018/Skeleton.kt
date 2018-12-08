512a564bb623ff0229151777501980ca765d78bd
Mattias Cederlund <mattec92@hotmail.com>
2018-12-08 12:37:50 +0100

    Add skeleton class.

---
+++ src/se/mattec/advent2018/Skeleton.kt
@@ -0,0 +1,22 @@
+package se.mattec.advent2018
+
+fun main(args: Array<String>) {
+    println(Skeleton.problem1())
+    println(Skeleton.problem2())
+}
+
+object Skeleton {
+
+    fun problem1(): String {
+        return ""
+    }
+
+    fun problem2(): String {
+        return ""
+    }
+
+    private val data = """
+
+    """.trimIndent()
+
+}
