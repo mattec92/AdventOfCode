f4f1d4e93ca05b6416c7734bb487579f85f3af09
Mattias Cederlund <mattec92@hotmail.com>
2018-12-16 00:36:59 +0100

    Day 14.

---
+++ src/se/mattec/advent2018/Day14.kt
@@ -0,0 +1,46 @@
+package se.mattec.advent2018
+
+fun main(args: Array<String>) {
+    println(Day14.problem1())
+    println(Day14.problem2())
+}
+
+object Day14 {
+
+    fun problem1(): String {
+        val startingFrom = 157901
+        val recipes = mutableListOf(3, 7)
+
+        var elfA = 0
+        var elfB = 1
+
+        for (i in 0 until startingFrom + 10) {
+            val newRecipe = recipes[elfA] + recipes[elfB]
+            recipes += newRecipe.toString().toCharArray().map { "$it".toInt() }
+            elfA = (elfA + 1 + recipes[elfA]) % recipes.size
+            elfB = (elfB + 1 + recipes[elfB]) % recipes.size
+        }
+
+        return recipes.subList(startingFrom, startingFrom + 10).joinToString("")
+    }
+
+    fun problem2(): Int {
+        val toFind = "157901"
+        val recipes = mutableListOf(3, 7)
+
+        var elfA = 0
+        var elfB = 1
+
+        while (true) {
+            val newRecipe = recipes[elfA] + recipes[elfB]
+            recipes += newRecipe.toString().toCharArray().map { "$it".toInt() }
+            elfA = (elfA + 1 + recipes[elfA]) % recipes.size
+            elfB = (elfB + 1 + recipes[elfB]) % recipes.size
+
+            if (recipes.size >= toFind.length + 2 && recipes.subList(recipes.size - toFind.length - 2, recipes.size).joinToString("").contains(toFind)) {
+                return recipes.size - toFind.length - 2 + recipes.subList(recipes.size - toFind.length - 2, recipes.size).joinToString("").indexOf(toFind)
+            }
+        }
+    }
+
+}
