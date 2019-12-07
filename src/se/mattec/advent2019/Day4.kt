1ac459486c2c393bbd147691afbfc5b87d010e91
Mattias Cederlund <mattec92@hotmail.com>
2019-12-07 13:34:57 +0100

    Day 4.

---
+++ src/se/mattec/advent2019/Day4.kt
@@ -0,0 +1,54 @@
+package se.mattec.advent2019
+
+fun main(args: Array<String>) {
+    println(Day4.problem1())
+    println(Day4.problem2())
+}
+
+object Day4 {
+
+    private val min = 246540
+    private val max = 787419
+    private val range = (0..9).map { it.toString() }
+
+    fun problem1(): Int {
+        var count = 0
+        for (password in min..max) {
+            if (isValid1(password.toString())) {
+                count++
+            }
+        }
+        return count
+    }
+
+    private fun isValid1(input: String): Boolean {
+        val hasAdjacent = range.any { digit -> input.contains("$digit$digit") }
+
+        val isIncreasing = !input.mapIndexed { index, digit ->
+            !(index < input.length - 1 && input[index + 1].toInt() < digit.toInt())
+        }.any { !it }
+
+        return hasAdjacent && isIncreasing
+    }
+
+    fun problem2(): Int {
+        var count = 0
+        for (password in min..max) {
+            if (isValid2(password.toString())) {
+                count++
+            }
+        }
+        return count
+    }
+
+    private fun isValid2(input: String): Boolean {
+        val hasAdjacent = range.any { digit -> input.contains("$digit$digit") && !input.contains("$digit$digit$digit") }
+
+        val isIncreasing = !input.mapIndexed { index, digit ->
+            !(index < input.length - 1 && input[index + 1].toInt() < digit.toInt())
+        }.any { !it }
+
+        return hasAdjacent && isIncreasing
+    }
+
+}
